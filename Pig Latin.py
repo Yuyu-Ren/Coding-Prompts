557b7e42b76952eb5ad8e2889c7c5fba4b721ffa
Yuyu <yuyu.ren343@hotmail.com>
2017-07-17 16:50:44 -0600

    Finished Coding

--- Pig Latin.py
+++ Pig Latin.py
@@ -1,1 +1,16 @@
-ooik
+input = raw_input("Word to Pig-Latinify: ")
+
+for x in xrange(len(input)):
+    if input[x] in ('aeiou'):
+        firstVowelPosition = x
+        firstVowel = input[x]
+        break
+
+newWord = input.split(firstVowel, 1)
+pigLatin = firstVowel+newWord[1]+newWord[0]
+
+
+if firstVowelPosition != 0: #non first-vowel word
+    print pigLatin+"ay"
+else: #first-vowel word
+    print pigLatin+"way"
