b4c584770f062e881de2c6b895460289a034942b
Yuyu <yuyu.ren343@hotmail.com>
2017-07-18 17:31:18 -0600

    Finished Coding

---
+++ Check if Palindrome.py
@@ -0,0 +1,28 @@
+input = raw_input("Palindrome Checker: ")
+length = len(input)
+
+def checkpalindrome():
+    secondCutDecrement = len(splitInput[1]) - 1
+    for x in splitInput[0]:
+        if x == splitInput[1][secondCutDecrement]:
+            secondCutDecrement -= 1
+            continue
+        else:
+            return "Not Palindrome!"
+
+    else:
+        return "Palindrome!"
+
+
+if length % 2 is 0:
+    # even-count word length
+    length = len(input) / 2  # location of slice
+    splitInput = input[:length], input[length:]  # slices input into two pieces
+    print checkpalindrome()
+
+else:
+    # uneven-count word length
+    length = len(input) / 2 # location of slice
+    splitInput = input[:length], input[length + 1:]  # slice into two pieces, omitting the middle letter
+    print checkpalindrome()
+
