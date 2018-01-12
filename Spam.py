64628bf82f8dc96c5e0efdcbc5c3ac694700bcf9
Yuyu Ren <8889046+Yuyu-Ren@users.noreply.github.com>
2018-01-12 01:08:40 -0500

    Added comments/Explanations.

--- Spam.py
+++ Spam.py
@@ -5,16 +5,16 @@ import time
 # enter relevant info here.
 key = ""
 channel_id = ""
-message =  ""
+message = ""
 
 
 def send_message(channel_id = "", message = "", key = ""):
-    command = '''curl 'https://discordapp.com/api/v6/channels/''' + str(
+    command = '''curl 'https://discordapp.com/api/v6/channels/''' + str(  # Just sends curl command to command line
         channel_id) + '''/messages' -H 'authorization: ''' \
               + key + ''' ' -H 'content-type: application/json' --data-binary '{"content":"''' + str(message) \
               + '''"}' --compressed'''
-    os.system(command)
+    os.system(command)  # sends the properly formatted curl command
 
 
 send_message(channel_id, message, key)
-time.sleep(10)
+time.sleep(10)  # Prevent flood delay from Discord from taking effect
